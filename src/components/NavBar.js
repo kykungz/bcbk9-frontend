9ff7ef19de29e019a90c704a37d47d06c856c053
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-12 15:25:09 +0700

    set background image.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import Background from '../assets/navbar.png'
 const Container = styled.div`
   width: 100vw;
   display: flex;
+  background: url(${prop => prop.src});
+  background-size: cover;
+  background-position: center;
+  height: 130px;
 `
 const Menu = styled.div`
   flex: 1;
   text-align: center;
-  padding: 25px 0px 15px 0px;
+  padding: 30px 0px 40px 0px;
 `
 const MenuName = styled.span`
   color: #d63031;
 `
 
 export default () => (
-  <Container>
+  <Container src={Background}>
     <Menu>
       <MenuName>TIMETABLE</MenuName>
     </Menu>
