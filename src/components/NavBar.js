3620d69f7a86ebe5994b303e69b780bf2c1da4bd
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-13 15:02:24 +0700

    make top dropdown.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import Background from '../assets/navbar.png'
+import Hamburger from '../assets/hamburger.svg'
+import FacebookIcon from '../assets/facebookIcon.png'
+import TwitterIcon from '../assets/twitterIcon.svg'
 const Container = styled.div`
   width: 100vw;
   display: flex;
@@ -20,22 +23,43 @@ const Menu = styled.div`
   text-align: center;
   padding: 30px 0px 40px 0px;
   @media (max-width: 600px) {
-    justify-content: center;
     display: flex;
-    padding: 0px;
+    padding: 0px 0px 0px 30px;
   }
 `
 const MenuName = styled.span`
   color: #d63031;
   @media (max-width: 600px) {
-    padding-top: 25px;
+    padding-top: 20px;
     color: white;
     box-sizing: border-box;
   }
 `
-
+const TopDropDown = styled.div`
+  flex: 1;
+  background-color: white;
+  display: none;
+  @media (max-width: 600px) {
+    display: block;
+  }
+`
+const Button = styled.button`
+  background: url(${prop => prop.url});
+  background-position: center;
+  background-size: ${prop => prop.size};
+  background-repeat: no-repeat;
+  float: ${prop => prop.float};
+  width: 60px;
+  height: 60px;
+  border: none;
+`
 export default () => (
   <Container src={Background}>
+    <TopDropDown>
+      <Button url={Hamburger} float="left" size="80% 80%" />
+      <Button url={TwitterIcon} float="right" size="50% 50%" />
+      <Button url={FacebookIcon} float="right" size="50% 50%" />
+    </TopDropDown>
     <Menu>
       <MenuName>TIMETABLE</MenuName>
     </Menu>
