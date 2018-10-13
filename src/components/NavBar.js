4dea3c1dee587ad313d622422ed938e2272c9714
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-13 14:07:27 +0700

    make dropdown elements.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,14 +8,30 @@ const Container = styled.div`
   background-size: cover;
   background-position: center;
   height: 130px;
+  @media (max-width: 600px) {
+    height: 300px;
+    flex-direction: column;
+    background: none;
+    background-image: linear-gradient(#ff7675, #d63031);
+  }
 `
 const Menu = styled.div`
   flex: 1;
   text-align: center;
   padding: 30px 0px 40px 0px;
+  @media (max-width: 600px) {
+    justify-content: center;
+    display: flex;
+    padding: 0px;
+  }
 `
 const MenuName = styled.span`
   color: #d63031;
+  @media (max-width: 600px) {
+    padding-top: 25px;
+    color: white;
+    box-sizing: border-box;
+  }
 `
 
 export default () => (
