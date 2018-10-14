f7add60f86b94a971ab639af905a2b66fef48757
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-14 19:57:57 +0700

    set up component.

---
+++ src/components/Sponsors.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade'
+export default () => (
+  <>
+    <Fade top>
+      <h1>hello</h1>
+    </Fade>
+    <Fade top>
+      <h1>hello</h1>
+    </Fade>
+    <Fade top>
+      <h1>hello</h1>
+    </Fade>
+  </>
+)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import HelloWorld from '../components/HelloWorld'
-
+//import HelloWorld from '../components/HelloWorld'
+import Sponsors from '../components/Sponsors'
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <HelloWorld />
+        <Sponsors />
       </div>
     )
   }
