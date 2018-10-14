c79e9e075c404f479d7a8dd6f6722a4f243e9b99
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-14 21:52:52 +0700

    set up structure.

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import Fade from 'react-reveal/Fade'
+//import Fade from 'react-reveal/Fade'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center
+  flex-direction: column;
+`
+const PairContainer = styled.div`
+  width: 69%;
+  display: flex;
+  justify-content: space-between;
+  padding: calc(69% / 20) 0px;
+`
+const SponsorImage = styled.div`
+  background-color: red;
+  height: 20vw;
+  width: 45%;
+  border-radius: 20px;
+  @media (max-width: 650px) {
+    border-radius: 10px;
+  }
+`
 export default () => (
-  <>
-    <Fade top>
-      <h1>hello</h1>
-    </Fade>
-    <Fade top>
-      <h1>hello</h1>
-    </Fade>
-    <Fade top>
-      <h1>hello</h1>
-    </Fade>
-  </>
+  <Container>
+    <PairContainer>
+      <SponsorImage />
+      <SponsorImage />
+    </PairContainer>
+    <PairContainer>
+      <SponsorImage />
+      <SponsorImage />
+    </PairContainer>
+  </Container>
 )
