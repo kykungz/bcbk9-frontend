a6a5703ef1d78dc1cbfa8cb75a2e461d85793111
Anawin Wongsadit <imonmac@hotmail.com>
2018-10-14 12:08:02 +0700

    Remove scss, add responsive

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,35 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import './Content.scss'
 import line05 from '../assets/line05.png'
 const Section = styled.div`
   margin-bottom: 0px;
   background: url(${line05}) no-repeat top left;
   background-size: 28px;
+
+  @media (max-width: 650px) {
+    background-size: 26px;
+  }
 `
 const STitle = styled.h3`
   font-family: 'Century Gothic', 'Prompt';
   font-size: 28px;
   text-transform: uppercase;
   margin: 0;
-  margin-left: 60px;
+  margin-left: 55px;
+  @media (max-width: 650px) {
+    font-size: 24px;
+    margin-left: 45px;
+  }
 `
 const SContent = styled.div`
   font-family: 'Century Gothic', 'Prompt';
-  margin-left: 60px;
+
+  margin-left: 55px;
   padding-top: 15px;
   padding-bottom: 50px;
-`
-class Content extends React.Component {
-  render() {
-    return (
-      <Section>
-        <STitle>{this.props.title}</STitle>
-        <SContent className="SContent">{this.props.children}</SContent>
-      </Section>
-    )
+
+  p {
+    text-indent: 2em;
   }
-}
+  @media (max-width: 650px) {
+    font-size: 14px;
+
+    margin-left: 45px;
+    padding-top: 5px;
+    p {
+      text-indent: 0;
+    }
+  }
+`
+const Content = props => (
+  <Section>
+    <STitle>{props.title}</STitle>
+    <SContent className="SContent">{props.children}</SContent>
+  </Section>
+)
 
 export default Content
