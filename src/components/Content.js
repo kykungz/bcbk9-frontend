4e2c707d1b70e8149efb24d2da92adcb60c5879f
Anawin Wongsadit <imonmac@hotmail.com>
2018-10-15 23:55:47 +0700

    Fix style-related

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import line05 from '../assets/line05.png'
 const Section = styled.div`
   margin-bottom: 0px;
+  padding-left: 55px;
   background: url(${line05}) no-repeat top left;
   background-size: 28px;
 
@@ -15,14 +16,12 @@ const STitle = styled.h3`
   font-size: 28px;
   text-transform: uppercase;
   margin: 0;
-  margin-left: 55px;
   @media (max-width: 650px) {
     font-size: 24px;
     margin-left: 45px;
   }
 `
 const SContent = styled.div`
-  margin-left: 55px;
   padding-top: 15px;
   padding-bottom: 50px;
 
@@ -42,7 +41,7 @@ const SContent = styled.div`
 const Content = props => (
   <Section>
     <STitle>{props.title}</STitle>
-    <SContent className="SContent">{props.children}</SContent>
+    <SContent>{props.children}</SContent>
   </Section>
 )
 
