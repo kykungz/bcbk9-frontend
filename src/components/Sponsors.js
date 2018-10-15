f0ca62953594bc2f96b640c57613f5b49db68bed
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-15 19:56:04 +0700

    fix layout.

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -12,61 +12,61 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-`
-const PairContainer = styled.div`
-  width: 69%;
-  display: flex;
-  justify-content: space-between;
-  padding: calc(69% / 20) 0px;
-
+  margin-top: ${prop =>
+    prop.isSep ? `calc(69vw / 20 * ${prop.size})` : '0px'};
   @media (max-width: 650px) {
+    align-items: center;
     flex-direction: column;
-    padding: 0px;
   }
 `
-const SponsorImage = styled.div`
-  background: url(${prop => prop.url});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
+const SponsorImage = styled.img`
   background-color: white;
-  height: 20vw;
-  width: calc(69vw * 45 / 100);
+  height: calc(69vw * 45 / 150 * ${prop => prop.size});
+  width: calc(69vw * 45 / 100 * ${prop => prop.size});
   border-radius: 2vw;
+  margin: calc(69vw / 20 * ${prop => prop.size});
   @media (max-width: 650px) {
     border-radius: 3vw;
-    width: 69vw;
-    height: 34vw;
-    margin: calc(69vw / 20) 0px;
+    width: calc(69vw * ${prop => prop.size});
+    height: calc(69vw / 1.5 * ${prop => prop.size});
+    margin: calc(69vw / 20 * ${prop => prop.size}) 0px;
   }
 `
 
 const SponsorImageUrl = [
-  [Sponsor, Sponsor],
-  [Sponsor, Sponsor],
-  [Sponsor, Sponsor],
-  [Sponsor, Sponsor],
-  [Sponsor, Sponsor],
-  [Sponsor, Sponsor],
-  [Sponsor, Sponsor],
+  { urls: [Sponsor, Sponsor], size: 1 },
+  { urls: [Sponsor, Sponsor], size: 1 },
+  { urls: [Sponsor, Sponsor], size: 0.9 },
+  { urls: [Sponsor, Sponsor], size: 0.9 },
+  { urls: [Sponsor, Sponsor], size: 0.8 },
+  { urls: [Sponsor, Sponsor], size: 0.8 },
+  { urls: [Sponsor, Sponsor], size: 0.7 },
+  { urls: [Sponsor, Sponsor], size: 0.7 },
+  { urls: [Sponsor, Sponsor], size: 0.6 },
+  { urls: [Sponsor, Sponsor], size: 0.6 },
+  { urls: [Sponsor, Sponsor], size: 0.5 },
+  { urls: [Sponsor, Sponsor], size: 0.5 },
 ]
 
-const PairSponsor = (a, b) => (
-  <Container>
-    <PairContainer>
-      <Fade bottom>
-        <Zoom>
-          <SponsorImage url={a} />
-        </Zoom>
-      </Fade>
-      <Fade bottom>
-        <Zoom>
-          <SponsorImage url={b} />
-        </Zoom>
-      </Fade>
-    </PairContainer>
+const PairSponsor = (a, i, sep) => (
+  <Container size={a.size} isSep={sep} key={i}>
+    <Fade bottom>
+      <Zoom>
+        <SponsorImage src={a.urls[0]} size={a.size} />
+      </Zoom>
+    </Fade>
+    <Fade bottom>
+      <Zoom>
+        <SponsorImage src={a.urls[1]} size={a.size} />
+      </Zoom>
+    </Fade>
   </Container>
 )
 export default () => (
-  <Background>{SponsorImageUrl.map(e => PairSponsor(e[0], e[1]))}</Background>
+  <Background>
+    {SponsorImageUrl.map((e, i, arr) => {
+      if (i === 0 || e.size !== arr[i - 1].size) return PairSponsor(e, i, true)
+      return PairSponsor(e, i, false)
+    })}
+  </Background>
 )
