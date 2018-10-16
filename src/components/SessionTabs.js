ad27534971e1c9293eb1c2ca17ad7f0322ccd088
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-16 14:45:07 +0700

    create Tab function.

--- src/components/SessionTabs.js
+++ src/components/SessionTabs.js
@@ -1,3 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+  width: 43vw;
+`
+const Tab = styled.div`
+  display: flex;
+  background-image: linear-gradient(to right, #f38b7b, #983a7a);
+  border-radius: 2vw;
+  width: 100%;
+  height: calc(43vw / 5.4);
+`
+const TextContainer = styled.div`
+  flex: 4.4;
+`
+const NumContainer = styled.div`
+  flex: 1;
+`
 
-export default () => <h1>ice</h1>
+const getTab = () => (
+  <Tab>
+    <TextContainer />
+    <NumContainer />
+  </Tab>
+)
+
+export default () => <Container>{getTab()}</Container>
