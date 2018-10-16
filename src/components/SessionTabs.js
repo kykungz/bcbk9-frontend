9c190ff4d1b6eb7a1e53e5d582242306a198dcf0
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-16 14:59:21 +0700

    set up layout and add number.

--- src/components/SessionTabs.js
+++ src/components/SessionTabs.js
@@ -12,15 +12,47 @@ const Tab = styled.div`
 `
 const TextContainer = styled.div`
   flex: 4.4;
+  display: flex;
+  flex-direction: column;
 `
 const NumContainer = styled.div`
   flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
-
+const Circle = styled.div`
+  background-color: white;
+  height: 85%;
+  width: 85%;
+  border-radius: 50%;
+  display: flex;
+  justufy-content: center;
+  align-items: center;
+`
+const SessionName = styled.span`
+  flex: 1;
+`
+const SpeakerName = styled.span`
+  flex: 1;
+`
+const Number = styled.span`
+  flex: 1;
+  text-align: center;
+  color: #ee4e4f;
+  font-size: 2vw;
+`
 const getTab = () => (
   <Tab>
-    <TextContainer />
-    <NumContainer />
+    <TextContainer>
+      <SessionName />
+      <SpeakerName />
+    </TextContainer>
+    <NumContainer>
+      <Circle>
+        <Number>17201</Number>
+      </Circle>
+    </NumContainer>
   </Tab>
 )
 
