e8ac2e0912eaf5ec0608411a005550f6838f7fcf
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-16 14:21:06 +0700

    set up component.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,11 @@
 import React from 'react'
 //import HelloWorld from '../components/HelloWorld'
-import Sponsors from '../components/Sponsors'
-import NavBar from '../components/NavBar'
-import Location from '../components/Location'
-
+import SessionTabs from '../components/SessionTabs'
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Location />
-        <Sponsors />
+        <SessionTabs />
       </div>
     )
   }
