2433554d825a99d9f2da6ddbbfad9b0fd6adc162
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-10-20 16:53:56 +0700

    :sparkles: Add Loading Logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import Logo from '../assets/logo.svg'
 import Barcamp from '../assets/barcamp.svg'
+import Loader from 'react-loaders'
+
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -9,20 +11,64 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
 `
-const DetailContainer = styled.div`
+
+const LogoComponent = styled.img`
+  width: 300px;
+`
+
+const LogoContainer = styled.div`
   display: flex;
   flex-direction: column;
+
+  > :first-child {
+    animation: FadeIn 3s;
+  }
+
+  > :last-child {
+    animation: FadeIn 2s;
+  }
+
+  @keyframes FadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 `
-const LogoComponent = styled.img`
-  width: 250px;
+
+const PulseLoader = styled(Loader)`
+  transform: scale(1.5);
 `
-export default () => (
-  <div>
-    <Container>
-      <DetailContainer>
-        <LogoComponent src={Logo} />
-        <LogoComponent src={Barcamp} />
-      </DetailContainer>
-    </Container>
-  </div>
-)
+
+export default class extends React.Component {
+  state = {
+    loading: true,
+  }
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        loading: false,
+      })
+    }, 2000)
+  }
+
+  render() {
+    return (
+      <div>
+        <Container>
+          {this.state.loading ? (
+            <PulseLoader color="#C14F6B" type="ball-pulse" />
+          ) : (
+            <LogoContainer>
+              <LogoComponent src={Logo} />
+              <LogoComponent src={Barcamp} />
+            </LogoContainer>
+          )}
+        </Container>
+      </div>
+    )
+  }
+}
