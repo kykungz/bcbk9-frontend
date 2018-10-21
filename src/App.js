afb6be2f21822d1ee7cefd33c7de639389543a1a
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-10-21 11:27:07 +0700

    :wheelchair: Improve accessibility

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import RouterView from './router'
-import background from './assets/background.png'
 
 const SPEED = 12
 const OFFSET = 400
@@ -10,7 +9,7 @@ class App extends Component {
   alterBackground = () => {
     if (window.scrollY < SPEED * 100 - OFFSET) {
       const opacity = (window.scrollY / (SPEED * 100)).toFixed(2)
-      document.body.style.backgroundImage = `linear-gradient(to bottom, rgba(255,255,255,${opacity}), rgba(255,255,255,${opacity})), url("${background}")`
+      document.body.style.setProperty('--opacity', 1 - opacity)
     }
   }
 
--- src/index.js
+++ src/index.js
@@ -8,13 +8,24 @@ import { injectGlobal } from 'styled-components'
 import background from './assets/background.png'
 
 injectGlobal`
-  body {
+  body:before {
+    content: "";
+    top: 0;
+    left: 0;
+    opacity: var(--opacity, 1);
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: fixed;
+    z-index: -1;
     background-image: url("${background}");
     background-attachment: fixed;
     background-position: bottom;
     background-size: cover;
     background-repeat: no-repeat;
   }
+
   * {
     font-family: 'Prompt', sans-serif;
   }
