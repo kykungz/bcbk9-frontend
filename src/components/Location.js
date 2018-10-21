b3fa285b38f7aa00252db87f93528ab1bdf3f1ff
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-21 18:23:32 +0700

    change location to IUP building.

--- src/components/Location.js
+++ src/components/Location.js
@@ -23,7 +23,7 @@ export default () => (
     </Zoom>
     <Zoom>
       <Map
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3873.8786118931484!2d100.56634103564576!3d13.84632421925175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29ce74be3ff67%3A0x740d29058aa90dcc!2sDepartment+of+Computer+Engineering%2C+Kasetsart+University!5e0!3m2!1sen!2sth!4v1539427242601"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d516.4958529942247!2d100.5696961920409!3d13.846490263227853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29ce0ba6b4eb7%3A0x2a0f9538c39c6511!2sIUP+International+Building+17!5e0!3m2!1sen!2sth!4v1540120842495"
         frameborder="0"
       />
     </Zoom>
