cbbac9b215a4bd7186d3857b8a3a07a269d1df3a
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-10-21 11:46:16 +0700

    Fix background opacity changing

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ const OFFSET = 400
 
 class App extends Component {
   alterBackground = () => {
+    let opacity
     if (window.scrollY < SPEED * 100 - OFFSET) {
-      const opacity = (window.scrollY / (SPEED * 100)).toFixed(2)
-      document.body.style.setProperty('--opacity', 1 - opacity)
+      opacity = (window.scrollY / (SPEED * 100)).toFixed(2)
+    } else {
+      opacity = (1 - OFFSET / (SPEED * 100)).toFixed(2)
     }
+    document.body.style.setProperty('--opacity', 1 - opacity)
   }
 
   componentDidMount = () => {
     window.addEventListener('scroll', this.alterBackground)
     this.alterBackground()
+    // if (window.scrollY >= SPEED * 100 - OFFSET) {
+    //   const opacity = (1 - OFFSET / (SPEED * 100)).toFixed(2)
+    //   document.body.style.setProperty('--opacity', 1 - opacity)
+    // }
   }
 
   render() {
