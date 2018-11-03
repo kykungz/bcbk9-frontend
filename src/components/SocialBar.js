e98589d252e673a0f24263ddba2d98574e052a08
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-11-03 21:22:49 +0700

    add animation to social button.

--- src/components/SocialBar.js
+++ src/components/SocialBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import { SocialIcon } from 'react-social-icons'
 import Popover from 'react-popover'
@@ -34,30 +34,52 @@ const PopContainer = styled.span`
   border-radius: 0.25em;
 `
 const body = url => <PopContainer>{url}</PopContainer>
-export default () => {
-  return (
-    <SocialBar>
-      <Popover
-        isOpen={true}
-        body={body('facebook.com/barcampbangkhen')}
-        preferPlace="end"
-      >
-        <SocialIcon url="https://facebook.com/barcampbangkhen" />
-      </Popover>
-      <Popover
-        isOpen={true}
-        body={body('https://twitter.com/barcampbangkhen')}
-        preferPlace="end"
-      >
-        <SocialIcon url="twitter.com/barcampbangkhen" />
-      </Popover>
-      <Popover
-        isOpen={true}
-        body={body('info@barcampbangkhen.org')}
-        preferPlace="end"
-      >
-        <SocialIcon url="mailto:info@barcampbangkhen.org" />
-      </Popover>
-    </SocialBar>
-  )
+export default class extends Component {
+  state = {
+    isOpen: [false, false, false],
+  }
+  onActive = (index, value) => () => {
+    let isOpen = this.state.isOpen
+    isOpen[index] = value
+    this.setState({ isOpen })
+  }
+  render = () => {
+    return (
+      <SocialBar>
+        <Popover
+          isOpen={this.state.isOpen[0]}
+          body={body('facebook.com/barcampbangkhen')}
+          preferPlace="end"
+        >
+          <SocialIcon
+            onMouseOver={this.onActive(0, true)}
+            onMouseOut={this.onActive(0, false)}
+            url="https://facebook.com/barcampbangkhen"
+          />
+        </Popover>
+        <Popover
+          isOpen={this.state.isOpen[1]}
+          body={body('https://twitter.com/barcampbangkhen')}
+          preferPlace="end"
+        >
+          <SocialIcon
+            onMouseOver={this.onActive(1, true)}
+            onMouseOut={this.onActive(1, false)}
+            url="twitter.com/barcampbangkhen"
+          />
+        </Popover>
+        <Popover
+          isOpen={this.state.isOpen[2]}
+          body={body('info@barcampbangkhen.org')}
+          preferPlace="end"
+        >
+          <SocialIcon
+            onMouseOver={this.onActive(2, true)}
+            onMouseOut={this.onActive(2, false)}
+            url="mailto:info@barcampbangkhen.org"
+          />
+        </Popover>
+      </SocialBar>
+    )
+  }
 }
